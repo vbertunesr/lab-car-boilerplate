cb89f10c940a17f59229a89a971bf27f1635f82d
Valeria Bertunes <bertunesrocha@gmail.com>
2018-07-31 01:08:07 -0300

    quase

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-// La funcionalidad de tu proyecto
+var map;
+var mapMobile;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -11.606968, lng: -77.059364},
+    zoom: 3
+  });
+  mapMobile = new google.maps.Map(document.getElementById('map-mobile'), {
+    center: {lat: -11.606968, lng: -77.059364},
+    zoom: 3
+  });
+}
