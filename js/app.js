b00812491deedf406a9d5fcac6aa25085231408b
Valeria Bertunes <bertunesrocha@gmail.com>
2018-07-31 16:12:27 -0300

    ready challenge

--- js/app.js
+++ js/app.js
@@ -1,12 +1,1 @@
-var map;
-var mapMobile;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -11.606968, lng: -77.059364},
-    zoom: 3
-  });
-  mapMobile = new google.maps.Map(document.getElementById('map-mobile'), {
-    center: {lat: -11.606968, lng: -77.059364},
-    zoom: 3
-  });
-}
+
